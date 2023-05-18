d04b9edc2f3f98254a4495ed7f443f7a2ef45f86
Yashwin Tapdiya <yshwintapdiya@gmail.com>
2023-05-18 13:59:39 +0530

    Added Middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore , applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+//curried form of logger function
+//function logger(obj, next, action)
+//logger(obj)(next)(action)
+const logger = function({ dispatch , getState }){
+  return function(next){
+    return function(action){
+      //middleware code
+      console.log('ACTION_TYPE =' , action.type)
+      next(action);
+    }
+  }
+}
+
+const store = createStore(rootReducer , applyMiddleware(logger));
 console.log('store' , store); 
 // console.log('BEFORE STATE' , store.getState());
 
